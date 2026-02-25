32f5bfb20667b6f1ee472e8ea87496ddf6982d61
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-02-25 16:05:34 +0100

    Extract OverlayTcpStream

--- ZTSharp/Sockets/OverlayTcpClient.cs
+++ ZTSharp/Sockets/OverlayTcpClient.cs
@@ -387,47 +387,4 @@ public sealed class OverlayTcpClient : IAsyncDisposable
         return BinaryPrimitives.ReadUInt64LittleEndian(buffer);
     }
 
-    private sealed class OverlayTcpStream : Stream
-    {
-        private readonly OverlayTcpClient _client;
-
-        public OverlayTcpStream(OverlayTcpClient client)
-        {
-            _client = client;
-        }
-
-        public override bool CanRead => true;
-        public override bool CanSeek => false;
-        public override bool CanWrite => true;
-        public override long Length => throw new NotSupportedException();
-        public override long Position { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
-
-        public override void Flush()
-        {
-        }
-
-        public override Task FlushAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
-        public override int Read(byte[] buffer, int offset, int count)
-            => ReadAsync(buffer.AsMemory(offset, count)).AsTask().GetAwaiter().GetResult();
-
-        public override async ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
-            => await _client.ReadAsync(buffer, cancellationToken).ConfigureAwait(false);
-
-        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-            => ReadAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
-
-        public override void Write(byte[] buffer, int offset, int count)
-            => WriteAsync(buffer.AsMemory(offset, count)).AsTask().GetAwaiter().GetResult();
-
-        public override async ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
-            => await _client.WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
-
-        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
-            => WriteAsync(buffer.AsMemory(offset, count), cancellationToken).AsTask();
-
-        public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
-
-        public override void SetLength(long value) => throw new NotSupportedException();
-    }
 }
